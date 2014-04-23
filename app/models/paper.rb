12499851561f2cc25d1f80f08cf128e9c0e1ee24
Ryan Lower <rpjlower@gmail.com>
2014-04-23 16:13:17 -0700

    Add scrape method for papers

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,30 @@ class Paper < ActiveRecord::Base
   has_many :authors
 
   validates :issue, presence: true
+
+  def scrape!
+    Timeout::timeout(30) do
+      scraper.scrape!
+    end
+
+    if scraper.title && scraper.author # Basic sanity check
+      doi = scraper.doi
+      title = scraper.title
+      published_at = scraper.published_at
+      keywords = scraper.keywords
+      authors.create! do |author|
+        author.name = scraper.author.name
+        author.email = scraper.author.email
+        author.location = scraper.author.location
+      end
+      paper.save!
+    end
+  end
+
+  private
+
+    def scraper
+      # TODO, make work for non BMC journals
+      @scraper ||= Journals::BmcPaper.new url: url
+    end
 end
