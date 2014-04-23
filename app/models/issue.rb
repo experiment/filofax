d43ae24e2277c8de663bc0adca442af5362f9068
Ryan Lower <rpjlower@gmail.com>
2014-04-23 16:08:37 -0700

    Add through associations

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,5 @@
 class Issue < ActiveRecord::Base
   belongs_to :journal
   has_many :papers
+  has_many :authors, through: :papers
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,3 +1,5 @@
 class Journal < ActiveRecord::Base
   has_many :issues
+  has_many :papers, through: :issues
+  has_many :authors, through: :papers
 end
