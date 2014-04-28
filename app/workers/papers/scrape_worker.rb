143d8ea3906d9131d3aacd208fc9223dfadea76a
Ryan Lower <rpjlower@gmail.com>
2014-04-28 10:07:57 -0700

    Always retry scraping in 1 day Rather than using sidekiqs default exponential backoff

--- app/workers/papers/scrape_worker.rb
+++ app/workers/papers/scrape_worker.rb
@@ -2,6 +2,10 @@ module Papers
   class ScrapeWorker
     include Sidekiq::Worker
 
+    sidekiq_retry_in do |count|
+      86400 * count # always retry in 1 day
+    end
+
     def perform(paper_id)
       paper = Paper.find paper_id
 
