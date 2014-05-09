83c9df2345bf0eee30be8a9cf96675e73c145c54
Ryan Lower <rpjlower@gmail.com>
2014-05-09 13:21:56 -0700

    Only attempt to scrape each paper once Pending Microryza/journals#4

--- app/workers/papers/scrape_worker.rb
+++ app/workers/papers/scrape_worker.rb
@@ -2,11 +2,13 @@ module Papers
   class ScrapeWorker
     include Sidekiq::Worker
 
-    sidekiq_retry_in do |count|
-      86400 * count # always retry in 1 day
-    end
+    # Currently not retrying
+    # sidekiq_retry_in do |count|
+    #   86400 * count # always retry in 1 day
+    # end
 
-    sidekiq_options throttle: { threshold: 1, period: 30.seconds }
+    sidekiq_options throttle: { threshold: 1, period: 30.seconds },
+      :retry => false
 
     def perform(paper_id)
       paper = Paper.find paper_id
